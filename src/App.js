9a6b5d747fcf46b459c951f993ef4e64190d30b6
Lasyanga <herminigildojrquiano@gmail.com>
2022-04-02 16:52:43 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
+import Analyzer from "./components/analyzer";
+import Intro from "./components/intro";
+import Navbar from "./components/navbar";
+import Reference from "./components/reference";
 
-function App(){
-  const heading = <h1>Sentiments</h1>;
-  return(
-    <>
-    {heading}
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Navbar />
+        <div className="container">
+          <Intro />
+          <Analyzer />
+          <Reference />
+        </div>
+      </>
+    );
+  }
 }
-
 
 export default App;
